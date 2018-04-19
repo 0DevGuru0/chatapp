1ee7de9c201b294ffa8d4a01e0fe8f3dac7d385e
sajjadhadafi <af.hadfi@gmail.com>
2018-04-19 08:45:21 +0430

    test server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path     = require('path')
 const http     = require('http')
 const socketIO = require('socket.io')
 const express  = require('express')
-
+const {generateMessage} = require('./utils/message')
 var app    = express()
 const port = process.env.PORT || 3000
 var server = http.createServer(app)
@@ -13,8 +13,17 @@ app.use(express.static(publicPath))
 
 io.on('connection',(socket)=>{
   console.log('New User Connected.')
+
   socket.on('disconnect',()=>{
     console.log('user was disconnected.')
+  })
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'))
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'))
+  //2//
+  socket.on('createMessage',(message)=>{
+    console.log('createMessage',message)
+    // socket.emit('newMessage',generateMessage(message.from,message.text))
+    socket.broadcast.emit('newMessage',generateMessage(message.from,message.text))
   })
 })
 
